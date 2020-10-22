74499cd99a27b0c4d214025da1b3e1c591b4e88b
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-22 19:28:48 +0300

    vb. net example

---
+++ DemoApp.VB/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim notify = New NotificationWindow.PopupNotifier()
+        notify.Image = My.Resources.icon
+        notify.ContentText = "test"
+        notify.Popup()
+    End Sub
+End Class
